4ba3843ee535f060790f35b15fa353476318856b
Alex Ellis <alexellis2@gmail.com>
2016-05-01 18:51:00 +0100

    White space

--- show_cow.js
+++ show_cow.js
@@ -4,12 +4,15 @@ var all = cows();
 var cowArgument = process.argv[2]
 if(!cowArgument || isNaN(Number(cowArgument))) {
   console.log("Give cow number as argument");
+
 }
 else {
+
   var cowNumber = Number(cowArgument);
   if(cowNumber && cowNumber <= all.length) {
     console.log(all[cowNumber-1]);
   } else {
     console.log("Enter cow number between [1-"+all.length+"]")
   }
+
 }
